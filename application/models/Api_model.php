4ee788a9b80e253c1aa25ab1501806516daed7cf
exaltedsol04 <exaltedsol04@gmail.com>
2026-03-24 12:38:58 +0530

    COMMENT

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -3,10 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Api_model extends CI_Model {
 	
-	public function __construct() {
+	/*public function __construct() {
         parent::__construct();
         //$this->load->database();
-    }
+    }*/
     public function get_all() {
         return $this->db->get(COMPANY_SETTINGS)->result_array();
     }
